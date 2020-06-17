050ff6a3f7d2685eedb0cc8565979f8b15378c93
GabrielDeCastro <30298508+GabrielDCastro@users.noreply.github.com>
2020-06-17 16:01:36 -0300

    update classificação de iris

--- Classificação/Classificacao_De_Iris.py
+++ Classificação/Classificacao_De_Iris.py
@@ -1,40 +1,55 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from IPython.display import Image
+import pydot
 from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
 import pydotplus
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier
+from IPython.display import Image
 
-#Nome das colunas colocamos referente ao que cada valor do csv representa
+
 nameColumns= ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Class']
-#Lendo o csv e dando nome as colunas
-dataFrame = pd.read_csv('iris.csv', names= nameColumns)
-print("Linhas: %d, Colunas: %d" % (len(dataFrame), len(dataFrame.columns)))
+dataFrame = pd.read_csv('iris.csv', names = nameColumns)
 
-#Pega só os 5 primeiros valores
-a= dataFrame.head()
+#Visualizar as prmeiras linhas da tabela
+print(dataFrame.head())
 
+#Contar quantas flores tem de cada categoria
+dataFrame['Class'].value_counts()
 
-#mostra em gráfico
+#Gerar gráfico para distribuir
 dataFrame['Class'].value_counts().plot(kind='pie')
 plt.show()
 
-dataFrame['SepalArea']=dataFrame['SepalLength'] * dataFrame['SepalWidth']
+#Criação de features
+dataFrame['SepalArea'] = dataFrame['SepalLength'] * dataFrame['SepalWidth']
+dataFrame['PetalArea'] = dataFrame['PetalLength'] * dataFrame['PetalWidth']
+print(dataFrame.head())
 
-#Exemplos do que se pode fazer com pandas
-dataFrame['SepalArea'].mean()   #média
-dataFrame['SepalArea'].min()    #mínimo
-dataFrame['SepalArea'].max()    #máximo
-dataFrame['SepalArea'].mode()   #moda
-dataFrame['SepalArea'].median() #mediana
+#Análise de dados estatísticos do data frameround(dataFrame['SepalArea'].mean(),2)
+print(round(dataFrame['SepalArea'].mean(),2))
+print(dataFrame['SepalArea'].min())
+print(dataFrame['SepalArea'].max())
+print(dataFrame['SepalArea'].mode())
+print(dataFrame['SepalArea'].median())
 
+#Criação demais features
+dataFrame['SepalLengthAboveMean'] = dataFrame['SepalLength'] > dataFrame['SepalLength'].mean()
+dataFrame['SepalWidthAboveMean'] = dataFrame['SepalWidth'] > dataFrame['SepalWidth'].mean()
+dataFrame['PetalLengthAboveMean'] = dataFrame['PetalLength'] > dataFrame['PetalLength'].mean()
+dataFrame['PetalWidthAboveMean'] = dataFrame['PetalWidth'] > dataFrame['PetalWidth'].mean()
+dataFrame.head()
 
+#Recuperar o nome das colunas que formam as variáveis independentes
 features = dataFrame.columns.difference(['Class'])
-X = dataFrame[features].values  #Variáveis dependentes
-y = dataFrame['Class'].values   #Variáveis independentes
+#features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'SepalArea','PetalArea', 'SepalLengthAboveMean', 'SepalWidthAboveMean', 'PetalLengthAboveMean', 'PetalWidthAboveMean']
+print(features)
 
-#Colocando exemplos para serem testados na decisão. Serão esses três exemplos
+#Criar armazenadores para as variáveis indepentes (X) e para a dependente (y)
+X = dataFrame[features].values
+y = dataFrame['Class'].values
+print(X)
+
+#Criar variáveis com valores que não estão entre os do treinamento para averiguar se a árvore foi criada corretamente
 # Iris-setosa
 sample1 = [1.0, 2.0, 3.5, 1.0, 10.0, 3.5, False, False, False, False]
 # Iris-versicolor
@@ -42,15 +57,14 @@ sample2 = [5.0, 3.5, 1.3, 0.2, 17.8, 0.2, False, True, False, False]
 # Iris-virginica
 sample3 = [7.9, 5.0, 2.0, 1.8, 19.7, 9.1, True, False, True, True]
 
-sample = pd.DataFrame([sample1, sample2, sample3], columns = ['SepalLength','SepalWidth','PetalLength','PetalWidth','SepalArea','PetalArea','SepalLengthAboveMean','SepalWidthAboveMean','PetalLengthAboveMean','PetalWidthAboveMean'])
-sample = sample[features].values
+#Criação da árvore de decisão
+classifier_dt = DecisionTreeClassifier(random_state=10, criterion='gini',max_depth=3)
+classifier_dt.fit(X, y)
 
+#Testar se a árvore de decisão consegue classificar corretamente os exemplos
+classifier_dt.predict([sample1, sample2, sample3])
 
-#representação gráfica e descobrir a classificação
-classifier_dt=DecisionTreeClassifier(random_state=10, criterion='gini', max_depth=3)
-classifier_dt.fit(X,y) # efetuar o treinamento
-classifier_dt.predict(sample) # testar os resultados com o gabarito
-
+#Exibição da árvore de decisão que foi criada
 
 # Create DOT data
 dot_data = tree.export_graphviz(classifier_dt, out_file=None,
@@ -60,16 +74,4 @@ dot_data = tree.export_graphviz(classifier_dt, out_file=None,
 # Draw graph
 graph = pydotplus.graph_from_dot_data(dot_data)
 
-# Show graph
-
-print(classifier_dt.predict(sample))
-
-dataFrameCluster = dataFrame.drop(columns=['Class'])
-objAgrupador = KMeans(n_clusters=3,random_state=0)
-objAgrupador.fit(dataFrameCluster)
-dataFrame_clustered = objAgrupador.fit_predict(dataFrameCluster)
-print(dataFrame_clustered)
-results = dataFrame[['Class']].copy()
-results['clusterNumber'] = dataFrame_clustered
-pd.set_option('display.max_rows', 150)
-print(results)
+Image(graph.create_png())
